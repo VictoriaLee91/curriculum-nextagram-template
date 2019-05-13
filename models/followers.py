72c4bad859be77eef7d5936f9ecc9959df981416
Victoria Lee <vickyloon91@gmail.com>
2019-05-13 18:22:25 +0800

    added views.py for followers

--- models/followers.py
+++ models/followers.py
@@ -7,3 +7,13 @@ from playhouse.hybrid import hybrid_property
 class Followers(BaseModel):
     fans = pw.ForeignKeyField(User, backref="idols")
     idols = pw.ForeignKeyField(User, backref="fans")
+    approved = pw.BooleanField(default=False)
+
+    class Meta:
+        indexes = (
+            (('fan', 'idol'), True),
+        )
+
+    @hybrid_property
+    def is_approved(self):
+        return True if self.approved else False
