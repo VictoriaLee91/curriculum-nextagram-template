0c0613d24e0b5a377cf84f4b9b2ba2fca7f0c689
Victoria Lee <vickyloon91@gmail.com>
2019-05-14 11:40:14 +0800

    finished non-functional follow and following function

--- instagram_api/blueprints/users/views.py
+++ instagram_api/blueprints/users/views.py
@@ -1,9 +1,28 @@
 from flask import Blueprint
 
 users_api_blueprint = Blueprint('users_api',
-                             __name__,
-                             template_folder='templates')
+                                __name__,
+                                template_folder='templates')
+
 
 @users_api_blueprint.route('/', methods=['GET'])
 def index():
     return "USERS API"
+
+
+@users_api_blueprint('/<username', methods=['POST'])
+@jwt_required()  # read up on jwt in flask. install flask-jwt. follow documentation
+def show(username):
+    user = User.get_or_none(User.username == username)
+
+    if not user:
+        resp = {
+            'message': 'No user found with this username.'
+        }
+
+
+'user': {
+    'id': user.id,
+    'username': user.username,
+    'email': user.email
+}
